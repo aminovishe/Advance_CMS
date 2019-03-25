b3714a55d1ceee7b403a23d37c82643d7a8f15cf
aminovishe <aminhad32@gmail.com>
2019-03-25 23:52:36 +0100

    add : post area && sub navigation

--- phpBasics/index.php
+++ phpBasics/index.php
@@ -1,14 +1,116 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>The Advance CMS</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    
+    <meta charset="utf-8">
+    <meta name="author" content="Mohamed Amin Hadrich">
+    <meta name="description" content="this is a advance cms system developed with udemy">
+    <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon">
+    <title>The AminPress CMS</title>
+    <link href="https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/bootstrap.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-custom bg-transparent">
+    <a class="navbar-brand" href="#"><img class="logo" src="images/logo.png" alt="aminpress"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav m-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Software</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Music</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact us</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn btn-outline-dark my-2 my-sm-0" type="submit"><i class="fas fa-search"></i> Search
+            </button>
+        </form>
+    </div>
+</nav>
+<header>
+    <div class="container-fluid text-center">
+        <h1>The CMS Blog</h1>
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+    </div>
+</header>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-5 m-auto">
+            <nav class='navbar navbar-light navbar-expand-lg navbar-sub'>
+                <ul class='navbar-nav m-auto'>
+                    <li class="nav-item active"><a class="nav-link" href="#">Technology</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold" href="#">Science</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold" href="#">Big Data</a></li>
+                    <li class="nav-item"><a class="nav-link font-weight-bold" href="#">Business Intelligence</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col-8">
+            <div class="post">
+                <h1>The first post</h1>
+                <div class="meta small"><a href="#">06-05-2019</a> | <a href="#">Tachnology</a> | <a href="#">Mohamed
+                        Amin HADRICH</a></div>
+                <p class="lead text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+                    deleniti laborum
+                    sapiente sint! Amet iste provident sint sit soluta! Aliquam assumenda beatae dolor, facere hic optio
+                    quaerat recusandae reiciendis sint....</p>
+                <a href="#" class="badge badge-secondary">What is Technology ?</a>
+                <a href="#" class="badge badge-secondary">How Technology works?</a>
+                <a href="#" class="badge badge-secondary">Intro to the Technology</a>
+            </div>
+            <div class="post">
+                <h1>The second post</h1>
+                <div class="meta small"><a href="#">06-05-2019</a> | <a href="#">Tachnology</a> | <a href="#">Mohamed
+                        Amin HADRICH</a></div>
+                <p class="lead text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+                    deleniti laborum
+                    sapiente sint! Amet iste provident sint sit soluta! Aliquam assumenda beatae dolor, facere hic optio
+                    quaerat recusandae reiciendis sint....</p>
+                <a href="#" class="badge badge-secondary">What is Technology ?</a>
+                <a href="#" class="badge badge-secondary">How Technology works?</a>
+                <a href="#" class="badge badge-secondary">Intro to the Technology</a>
+            </div>
+            <div class="post">
+                <h1>The third post</h1>
+                <div class="meta small"><a href="#">06-05-2019</a> | <a href="#">Tachnology</a> | <a href="#">Mohamed
+                        Amin HADRICH</a></div>
+                <p class="lead text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+                    deleniti laborum
+                    sapiente sint! Amet iste provident sint sit soluta! Aliquam assumenda beatae dolor, facere hic optio
+                    quaerat recusandae reiciendis sint....</p>
+                <a href="#" class="badge badge-secondary">What is Technology ?</a>
+                <a href="#" class="badge badge-secondary">How Technology works?</a>
+                <a href="#" class="badge badge-secondary">Intro to the Technology</a>
+            </div>
+        </div>
+        <div class="col-4">
+
+        </div>
+    </div>
+</div>
 </body>
 </html>
